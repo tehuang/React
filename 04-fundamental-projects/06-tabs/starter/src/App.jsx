df76361ccf8bef2bc61b979f5840c54dfaf36d16
Teresa Huang <teresayhhuang@gmail.com>
2023-10-23 12:35:19 -0400

    Tabs

---
+++ 04-fundamental-projects/06-tabs/starter/src/App.jsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+import JobInfo from "./JobInfo";
+import BtnContainer from "./BtnContainer";
+const url = "https://course-api.com/react-tabs-project";
+
+const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+  const [currentItem, setCurrentItem] = useState(0);
+
+  const fetchJobs = async () => {
+    try {
+      const res = await fetch(url);
+      const newJobs = await res.json();
+      setJobs(newJobs);
+      setIsLoading(false);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section className="jobs-center">
+        <div className="loading"></div>
+      </section>
+    );
+  }
+  return (
+    <section className="jobs-center">
+      <BtnContainer
+        jobs={jobs}
+        currentItem={currentItem}
+        setCurrentItem={setCurrentItem}
+      />
+      <JobInfo jobs={jobs} currentItem={currentItem} />
+    </section>
+  );
+};
+export default App;
