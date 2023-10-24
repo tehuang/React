c7677117faf6672a9f361d12050262352fd25e79
Teresa Huang <teresayhhuang@gmail.com>
2023-10-24 15:08:20 -0400

    React Carousel

---
+++ 04-fundamental-projects/07-slider/starter/src/App.jsx
@@ -0,0 +1,10 @@
+import Carousel from "./Carousel";
+import SlickCarousel from "./SlickCarousel";
+const App = () => {
+  return (
+    <main>
+      <SlickCarousel />
+    </main>
+  );
+};
+export default App;
