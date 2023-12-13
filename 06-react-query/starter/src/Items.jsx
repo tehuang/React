c17062fd97fd19a34a4523c2ca44969531a70403
Teresa Huang <teresayhhuang@gmail.com>
2023-12-13 17:22:19 -0500

    first commit

---
+++ 06-react-query/starter/src/Items.jsx
@@ -0,0 +1,28 @@
+import SingleItem from "./SingleItem";
+import { useFetchTasks } from "./reactQueryCustomHooks";
+
+const Items = () => {
+  const { isLoading, isError, data } = useFetchTasks();
+  if (isLoading) {
+    return <p style={{ marginTop: "1rem " }}>Loading...</p>;
+  }
+
+  if (isError) {
+    return <p style={{ marginTop: "1rem " }}>There was an error...</p>;
+  }
+  // console.log(error);
+  // if (error) {
+  //   return <p style={{ marginTop: "1rem " }}>{error.message}</p>;
+  // }
+
+  console.log(data);
+
+  return (
+    <div className="items">
+      {data.taskList.map((item) => {
+        return <SingleItem key={item.id} item={item} />;
+      })}
+    </div>
+  );
+};
+export default Items;
---
+++ 09-mixmaster/starter/src/pages/HomeLayout.jsx
@@ -0,0 +1,17 @@
+import { Outlet, useNavigation } from "react-router-dom";
+import Navbar from "../components/Navbar";
+
+const HomeLayout = () => {
+  const navigation = useNavigation();
+  //   console.log(navigation);
+  const isPageLoading = navigation.state === "loading";
+  return (
+    <>
+      <Navbar />
+      <section className="page">
+        {isPageLoading ? <div className="loading" /> : <Outlet />}
+      </section>
+    </>
+  );
+};
+export default HomeLayout;
